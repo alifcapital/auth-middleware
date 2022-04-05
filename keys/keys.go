79293f14bf52c44e8a7cc0ecd4802e2577452a60
alisher_m <alisher0594@gmail.com>
2022-04-05 12:05:49 +0500

    add middleware

---
+++ keys/keys.go
@@ -0,0 +1,131 @@
+package keys
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/alifcapital/auth-middleware/config"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Options(
+	fx.Provide(NewPublicKey),
+	fx.Invoke(Worker),
+)
+
+type Params struct {
+	fx.In
+	Config config.Config
+}
+
+type PubKeyResponse struct {
+	Meta     Meta     `json:"meta"`
+	Response Response `json:"response"`
+}
+
+type Meta struct {
+	Error      bool   `json:"error"`
+	Message    string `json:"message"`
+	StatusCode uint64 `json:"statusCode"`
+}
+
+type Response struct {
+	PublicKey string `json:"public_key"`
+}
+
+func NewPublicKey(p Params) (*rsa.PublicKey, error) {
+
+	url := p.Config.GetString("PUB_KEY_URI")
+	data := p.Config.GetString("PUB_KEY_DATA")
+
+	return GetPublicKey(url, data)
+}
+
+func GetPublicKey(url, data string) (*rsa.PublicKey, error) {
+	req, err := http.NewRequest("POST", url, strings.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var response PubKeyResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Meta.Error {
+		err = errors.New("error retrieving public key")
+		return nil, err
+	}
+
+	pubPem, _ := pem.Decode([]byte(response.Response.PublicKey))
+	if pubPem == nil {
+		err = errors.New("error decoding public key")
+		return nil, err
+	}
+
+	if pubPem.Type != "PUBLIC KEY" {
+		err = errors.New("error type of public key")
+		return nil, err
+	}
+
+	var parsedKey interface{}
+	if parsedKey, err = x509.ParsePKIXPublicKey(pubPem.Bytes); err != nil {
+		return nil, err
+	}
+
+	var pubKey *rsa.PublicKey
+	var ok bool
+	if pubKey, ok = parsedKey.(*rsa.PublicKey); !ok {
+		err = errors.New("error getting public key")
+		return nil, err
+	}
+
+	return pubKey, nil
+}
+
+type WorkerParams struct {
+	fx.In
+	Config    config.Config
+	PublicKey *rsa.PublicKey
+}
+
+func Worker(w WorkerParams) {
+	var err error
+	url := w.Config.GetString("PUB_KEY_URI")
+	data := w.Config.GetString("PUB_KEY_DATA")
+
+	ticker := time.NewTicker(24 * time.Hour)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				w.PublicKey, err = GetPublicKey(url, data)
+				if err != nil {
+					log.Fatalf("unable to get public keys")
+				} else {
+					log.Println("public keys updated")
+				}
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+}
---
+++ middleware.go
@@ -0,0 +1,114 @@
+package middleware
+
+import (
+	"crypto/rsa"
+	"github.com/alifcapital/auth-middleware/config"
+	"github.com/alifcapital/auth-middleware/keys"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+	"gopkg.in/square/go-jose.v2/jwt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var Module = fx.Options(
+	fx.Provide(NewMiddleware),
+	config.Module,
+	keys.Module,
+)
+
+type Params struct {
+	fx.In
+
+	Keys   *rsa.PublicKey
+	Config config.Config
+}
+
+type Middleware interface {
+	Middleware(nextHandler gin.HandlerFunc, roles ...string) gin.HandlerFunc
+}
+
+type middleware struct {
+	keys   *rsa.PublicKey
+	config config.Config
+}
+
+func NewMiddleware(p Params) (Middleware, error) {
+
+	middlewareItem := &middleware{
+		keys:   p.Keys,
+		config: p.Config,
+	}
+
+	return middlewareItem, nil
+}
+
+type TokenPayload struct {
+	Issuer    string              `json:"iss,omitempty"`
+	Subject   int64               `json:"sub,omitempty"`
+	Expiry    int64               `json:"exp,omitempty"`
+	NotBefore int64               `json:"nbf,omitempty"`
+	IssuedAt  int64               `json:"iat,omitempty"`
+	ID        string              `json:"jti,omitempty"`
+	Username  string              `json:"username,omitempty"`
+	Roles     map[string][]string `json:"roles"`
+}
+
+func (m *middleware) Middleware(nextHandler gin.HandlerFunc, roles ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		raw := strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer ")
+
+		tok, err := jwt.ParseSigned(raw)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization failed: " + err.Error()})
+			return
+		}
+
+		var out TokenPayload
+
+		if err := tok.Claims(m.keys, &out); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token " + err.Error()})
+			return
+		}
+
+		if time.Unix(out.Expiry, 0).Before(time.Now()) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Expired Token"})
+			return
+		}
+
+		service := m.config.GetString("SERVICE_NAME")
+
+		if !HasRoles(service, roles, out.Roles) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "access denied, user does not have required roles"})
+			return
+		}
+
+		c.Set("id", out.Subject)
+		c.Set("username", out.Username)
+		c.Set("roles", out.Roles)
+
+		nextHandler(c)
+	}
+}
+
+func HasRoles(serviceName string, expectedRoles []string, actualRoles map[string][]string) bool {
+	if service, ok := actualRoles[serviceName]; ok {
+
+		var shouldHave = make(map[string]bool)
+		for _, role := range service {
+			shouldHave[role] = true
+		}
+
+		for _, role := range expectedRoles {
+			if _, ok := shouldHave[role]; !ok {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
