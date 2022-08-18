6985671e6ce6855522113a3ad1a6633d62d58804
Hasanzoda Muhammadiqbol <foretde1999@gmail.com>
2022-08-18 11:59:16 +0500

    fixed

--- middleware.go
+++ middleware.go
@@ -1,43 +1,38 @@
 package middleware
 
 import (
-	"crypto/rsa"
 	"github.com/alifcapital/auth-middleware/config"
-	"github.com/alifcapital/auth-middleware/keys"
+	//"github.com/dgrijalva/jwt-go"
+	"net/http"
+
+	//"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
 	"gopkg.in/square/go-jose.v2/jwt"
-	"net/http"
+
 	"strings"
-	"time"
 )
 
 var Module = fx.Options(
 	fx.Provide(NewMiddleware),
-	config.Module,
-	keys.Module,
 )
 
 type Params struct {
 	fx.In
-
-	Keys   *rsa.PublicKey
 	Config config.Config
 }
 
 type Middleware interface {
-	Middleware(nextHandler gin.HandlerFunc, roles ...string) gin.HandlerFunc
+	Middleware(nextHandler gin.HandlerFunc) gin.HandlerFunc
 }
 
 type middleware struct {
-	keys   *rsa.PublicKey
 	config config.Config
 }
 
 func NewMiddleware(p Params) (Middleware, error) {
 
 	middlewareItem := &middleware{
-		keys:   p.Keys,
 		config: p.Config,
 	}
 
@@ -45,19 +40,31 @@ func NewMiddleware(p Params) (Middleware, error) {
 }
 
 type TokenPayload struct {
-	Issuer    string              `json:"iss,omitempty"`
-	Subject   string              `json:"sub,omitempty"`
-	Expiry    int64               `json:"exp,omitempty"`
-	NotBefore int64               `json:"nbf,omitempty"`
-	IssuedAt  int64               `json:"iat,omitempty"`
-	ID        string              `json:"jti,omitempty"`
-	Username  string              `json:"username,omitempty"`
-	Roles     map[string][]string `json:"roles"`
+	Exp               int      `json:"exp"`
+	Iat               int      `json:"iat"`
+	AuthTime          int      `json:"auth_time"`
+	Jti               string   `json:"jti"`
+	Iss               string   `json:"iss"`
+	Sub               string   `json:"sub"`
+	Typ               string   `json:"typ"`
+	Azp               string   `json:"azp"`
+	Nonce             string   `json:"nonce"`
+	SessionState      string   `json:"session_state"`
+	Acr               string   `json:"acr"`
+	AllowedOrigins    []string `json:"allowed-origins"`
+	Scope             string   `json:"scope"`
+	Sid               string   `json:"sid"`
+	EmailVerified     bool     `json:"email_verified"`
+	Systems           []string `json:"systems"`
+	Name              string   `json:"name"`
+	PreferredUsername string   `json:"preferred_username"`
+	GivenName         string   `json:"given_name"`
+	FamilyName        string   `json:"family_name"`
+	Email             string   `json:"email"`
 }
 
-func (m *middleware) Middleware(nextHandler gin.HandlerFunc, roles ...string) gin.HandlerFunc {
+func (m *middleware) Middleware(nextHandler gin.HandlerFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		raw := strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer ")
 
 		tok, err := jwt.ParseSigned(raw)
@@ -68,47 +75,13 @@ func (m *middleware) Middleware(nextHandler gin.HandlerFunc, roles ...string) gi
 
 		var out TokenPayload
 
-		if err := tok.Claims(m.keys, &out); err != nil {
+		if err := tok.UnsafeClaimsWithoutVerification(&out); err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token " + err.Error()})
 			return
 		}
-
-		if time.Unix(out.Expiry, 0).Before(time.Now()) {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Expired Token"})
-			return
-		}
-
-		service := m.config.GetString("SERVICE_NAME")
-
-		if !HasRoles(service, roles, out.Roles) {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "access denied, user does not have required roles"})
-			return
-		}
-
-		c.Set("id", out.Subject)
-		c.Set("username", out.Username)
-		c.Set("roles", out.Roles)
+		c.Set("id", out.Sub)
+		c.Set("username", out.GivenName)
 
 		nextHandler(c)
 	}
-}
-
-func HasRoles(serviceName string, expectedRoles []string, actualRoles map[string][]string) bool {
-	if service, ok := actualRoles[serviceName]; ok {
-
-		var shouldHave = make(map[string]bool)
-		for _, role := range service {
-			shouldHave[role] = true
-		}
-
-		for _, role := range expectedRoles {
-			if _, ok := shouldHave[role]; !ok {
-				return false
-			}
-		}
-
-		return true
-	}
-
-	return false
 }
